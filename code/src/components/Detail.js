2bca9959bd72865ffd602dc1cc15f9665c8a7b6b
João Camba <joaonoeljose@gmail.com>
2023-06-11 19:59:15 +0100

    Similar videos section. Titles added and routing complete

--- code/src/components/Detail.js
+++ code/src/components/Detail.js
@@ -41,10 +41,13 @@ const Detail = ({ exerciseDetail }) => {
         </Typography>
         {extraDetail.map((item) => (
           <Stack key={item.name} direction='row' gap='24px' alignItems='center'>
-            <Button sx={{}}>
-              <img src={item.icon} />
+            <Button sx={{
+              background: '#fff2db', borderRadius: '50%', width: '100px',
+              height: '100px'
+            }}>
+              <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }} />
             </Button>
-            <Typography variant='h5'>
+            <Typography variant='h5' textTransform='capitalize'>
               {item.name}
             </Typography>
           </Stack>
